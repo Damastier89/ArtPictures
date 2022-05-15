53e9d6ccb0004f7cc030bd51b75f56eadc176437
Damastier <example@mail.ru>
2022-05-15 17:44:31 +0300

    hidden showModalByTime

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -75,7 +75,7 @@ const modals = () => {
   
   bindModal('.button-design', '.popup-design', '.popup-design .popup-close');
   bindModal('.button-consultation', '.popup-consultation', '.popup-consultation .popup-close');
-  showModalByTime('.popup-consultation', 5000);
+  // showModalByTime('.popup-consultation', 60000);
 };
 
 export default modals;
